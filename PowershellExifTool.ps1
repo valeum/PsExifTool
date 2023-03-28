91f9914d48dcc84771d56a4c90f325688ce5ffe1
VALEUM <85646451+valeum@users.noreply.github.com>
2023-03-28 10:05:48 +0200

    Update PowershellExifTool.ps1

--- PowershellExifTool.ps1
+++ PowershellExifTool.ps1
@@ -1,3 +1,45 @@
+function Install-ExifTool {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$false)]
+        [string]$InstallPath = $PSScriptRoot,
+        [Parameter(Mandatory=$false)]
+        [string]$DownloadUrl
+    )
+
+    $ZipPath = Join-Path -Path $env:TEMP -ChildPath 'exiftool.zip'
+    $ExifToolPath = Join-Path -Path $InstallPath -ChildPath 'exiftool.exe'
+
+    if (Test-Path $ExifToolPath) {
+        Write-Output "ExifTool already installed at $ExifToolPath."
+        return
+    }
+
+    if (-not (Test-Path $InstallPath)) {
+        Write-Error -Message "The specified path '$InstallPath' does not exist or is not accessible." -ErrorAction Stop
+    }
+
+    if(-not ($DownloadUrl)) {
+        Write-Output "Get newest ExifTool download url."
+        [xml]$Rss = Invoke-WebRequest -Uri 'https://exiftool.org/rss.xml' -UseBasicParsing
+        $DownloadUrl = ($Rss.rss.channel.item.enclosure | Where-Object {$_.type -eq 'application/zip'} | Select-Object -First 1).url
+    }
+
+    Write-Output "Downloading ExifTool from $DownloadUrl."
+    Invoke-WebRequest -Uri $DownloadUrl -OutFile $ZipPath
+
+    Write-Output "Extracting ExifTool archive to $InstallPath."
+    Expand-Archive -Path $ZipPath -DestinationPath $InstallPath -Force | Out-Null
+
+    Write-Output "Cleaning up temporary files."
+    Remove-Item $ZipPath
+
+    Write-Host "Renaming ExifTool binary to exiftool.exe..."
+    Rename-Item -Path (Join-Path -Path $InstallPath -ChildPath 'exiftool(-k).exe') -NewName $ExifToolPath
+
+    Write-Output "ExifTool installed successfully at $ExifToolPath."
+}
+
 function Get-ExifData {
     [CmdletBinding()]
     param (
